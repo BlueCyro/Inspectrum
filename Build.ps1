0cb174229efd3f276faa417e878bb7121dbc3ff8
Cyro <BlueCyro@protonmail.com>
2022-03-05 13:24:15 -0600

    Initial commit

---
+++ Build.ps1
@@ -0,0 +1,22 @@
+dotnet build
+
+$dir = Split-Path -Path (Get-Location) -Leaf
+$NeosDir = "C:\Neos\app\"
+$NeosExe = "$NeosDir\Neos.exe"
+$AssemblyLocation = "$(Get-Location)\bin\Debug\net4.7.2\$dir.dll"
+$nml_mods = "$NeosDir\nml_mods\"
+
+Copy-Item -Force -Path $AssemblyLocation -Destination $nml_mods
+
+$LogJob = Start-Job {Start-Sleep -Seconds 8
+    Get-Content "C:\Neos\app\Logs\$(Get-ChildItem -Path C:\Neos\app\Logs | Sort-Object LastWriteTime | Select-Object -last 1)" -Wait
+}
+
+$NeosProc = Start-Process -FilePath $NeosExe -WorkingDirectory $NeosDir -ArgumentList "-DontAutoOpenCloudHome", "-SkipIntroTutorial", "-Screen", "-LoadAssembly `"C:\Neos\app\Libraries\NeosModLoader.dll`"" -passthru
+
+while(!$NeosProc.HasExited) {
+    Start-Sleep -Seconds 1
+    Receive-Job $LogJob.Id
+}
+
+Stop-Job $LogJob.Id
---
+++ Inspectrum.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+using FrooxEngine.LogiX;
+using BaseX;
+using CodeX;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+//Idea by Gareth48
+
+namespace YourNamespaceHere;
+public class ModClass : NeosMod
+{
+    public override string Author => "Cyro";
+    public override string Name => "Inspectrum";
+    public override string Version => "1.0.0";
+
+    public override void OnEngineInit()
+    {
+        Harmony harmony = new Harmony("net.Cyro.Inspectrum");
+        harmony.PatchAll();
+    }
+
+
+    [HarmonyPatch(typeof(InspectorPanel), "Setup")]
+    class InspectrumPatcher
+    {
+        static void Postfix(InspectorPanel __instance, NeosPanel __result)
+        {
+            Slot VisualConfigs = __instance.Slot.AddSlot("VisualConfigs");
+            VisualConfigs.OrderOffset = 1024;
+            
+            var cloudDriver = VisualConfigs.AttachComponent<CloudValueVariableDriver<color>>();
+            var NeosUIStyle = VisualConfigs.AttachComponent<NeosUIStyle>();
+            
+            cloudDriver.FallbackValue.Value = RandomX.RGB;
+            cloudDriver.Path.Value = "G-Neos.CustomUserColor";
+            cloudDriver.Target.Target = NeosUIStyle.Color;
+            cloudDriver.OverrideOwner.Value = __instance.World.LocalUser.UserID;
+
+            cloudDriver.Changed += (IChangeable c) =>
+            {
+                UniLog.Log(c.GetType());
+            };
+            
+            __result.Style = NeosUIStyle;
+            __result.Color = __result.Color.SetA(0f);
+            __result.RunInUpdates(3, () => __result.MarkChangeDirty());
+        }
+    }
+}
